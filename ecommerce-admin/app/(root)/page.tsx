533d9d78f435d1f5c2a6671f68afe70155f615eb
anon-pradip <pradipsubedi360@gmail.com>
2023-07-05 21:36:44 +0545

    initiate ecommerce-admin

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>hello admin dashboard</h1>
+      <Button className="ml-5" size="lg">
+        Hello
+      </Button>
+    </>
+  );
+}
