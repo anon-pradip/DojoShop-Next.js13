e6c64199f628cb857560531ea8aa11e223b8ea39
anon-pradip <pradipsubedi360@gmail.com>
2023-07-26 08:51:03 +0545

    make cloudinary image upload skeleton

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -24,8 +24,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import AlertModal from "@/components/modals/alert-modal";
-import ApiAlert from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
 
 interface BillboardformProps {
   initialData: Billboard | null;
@@ -113,6 +113,30 @@ const BillboardForm: React.FC<BillboardformProps> = ({ initialData }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-5 w-full"
         >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                  {/* <Input
+                    disabled={loading}
+                    placeholder="Billboard label"
+                    {...field}
+                  /> */}
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
@@ -138,11 +162,6 @@ const BillboardForm: React.FC<BillboardformProps> = ({ initialData }) => {
         </form>
       </Form>
       <Separator />
-      <ApiAlert
-        title="test"
-        description={`${origin}/${params.storeId}`}
-        variant="public"
-      />
     </>
   );
 };
