019e5bd50e4db3c4846621cb3976b6ba4bdd7aaa
anon-pradip <pradipsubedi360@gmail.com>
2023-07-31 11:40:59 +0545

    make api routes for sizes

---
+++ ecommerce-admin/app/api/[storeId]/sizes/route.ts
@@ -0,0 +1,82 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { storeId } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismadb.size.create({
+      data: {
+        name,
+        value,
+        storeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZES_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { storeId } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    const sizes = await prismadb.size.findMany({
+      where: {
+        storeId,
+      },
+    });
+
+    return NextResponse.json(sizes);
+  } catch (error) {
+    console.log("[Sizes_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
