2e7ef465eb43d329fe461d31f514c5d98fe73863
anon-pradip <pradipsubedi360@gmail.com>
2023-08-15 16:21:24 +0545

    make product choice options

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -4,6 +4,8 @@ import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import Container from "@/components/ui/container";
 import ProductList from "@/components/product-list";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 
 interface ProductPageProps {
   params: { productId: string };
@@ -14,18 +16,17 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
   const suggestedProducts = await getProducts({
     categoryId: product?.category?.id,
   });
-  console.log("product: ", product);
-  console.log("suggested:", suggestedProducts);
+  console.log("images: ", product.images);
   return (
     <div className=" bg-white">
       <Container>
         <div className="px-2 py-6 sm:px-4 lg:px-5">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             {/* Gallery */}
-            Gallery
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               {/* Info */}
-              Info
+              <Info data={product} />
             </div>
           </div>
           <hr className=" my-10" />
